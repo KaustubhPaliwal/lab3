a03c4ed0b95c63e454e68f6d8bfb6147a7c1ab3b
KaustubhPaliwal <122563165+KaustubhPaliwal@users.noreply.github.com>
2023-01-26 11:43:22 -0800

    .

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,12 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = {1,2,3,4};
     assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {1.0,1.0,3.0};
+    double avg = ArrayExamples.averageWithoutLowest(input1);
+    assertEquals(2.0,avg,0.00001);
   }
 }
